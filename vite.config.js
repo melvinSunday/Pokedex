322ec7c2c5661825566cc35874c2237cf9a74f33
Melvin Giovanni Domingo <domingotaiga@gmail.com>
2024-09-20 16:54:53 +0800

    n

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      antd: "antd/es",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['antd'],
+    },
+  },
+  optimizeDeps: {
+    include: ['antd'],
+  },
+
+ 
 })
